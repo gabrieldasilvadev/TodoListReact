0d49aa8e73fb16ea6fc76ce24bbf7c91c5391c72
gabriedasilvadev <gabrieldasilvaprivado1@gmail.com>
2022-01-06 18:30:31 -0300

    Add router for create todo and todo list(home)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddTodo from "./components/AddTodo";
 import Navbar from "./components/Navbar";
 import TodoList from "./components/TodoList";
 import TodoContext from "./contexts/TodoContext";
@@ -7,11 +9,23 @@ const App = () => {
   return (
     <TodoContext>
       <div className="uk-container">
-        <Navbar></Navbar>
-        <TodoList></TodoList>
+        <BrowserRouter>
+          <Navbar></Navbar>
+          <br />
+          <Routes>
+            <Route path="/" element={<TodoList />}></Route>
+
+            <Route path="/create" element={<AddTodo />}></Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </TodoContext>
   );
 };
 
 export default App;
+
+{
+  /* <Navbar></Navbar>
+<TodoList></TodoList> */
+}
