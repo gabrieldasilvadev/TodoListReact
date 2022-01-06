49da33d5ba72123abadfa7718d80b513c6bb4063
gabriedasilvadev <gabrieldasilvaprivado1@gmail.com>
2022-01-06 15:26:54 -0300

    Add provider of the TodoContext

--- src/contexts/TodoContext.tsx
+++ src/contexts/TodoContext.tsx
@@ -1,9 +1,42 @@
 import React, { createContext } from "react";
+import { Todo } from "../models/Todo";
 import { TodoContextType } from "./TodoContextType";
 
 export const TodoContext = createContext<TodoContextType>({
-  todos: [],
+  todos: [
+    { id: 1, title: "Estudar React", done: true },
+    { id: 2, title: "Estudar Java", done: false },
+  ],
   addTodo: () => {},
   removeTodo: () => {},
   toggle: () => {},
 });
+
+const TodoProvider = (props: any) => {
+  const addTodo = (title: string) => {
+    console.log(`Adicionou ${title}`);
+  };
+
+  const removeTodo = (todo: Todo) => {
+    console.log(`Removeu ${todo.title}`);
+  };
+
+  const toggle = (todo: Todo) => {
+    console.log(`Alterou ${todo.title}`);
+  };
+
+  return (
+    <TodoContext.Provider
+      value={{
+        todos,
+        addTodo,
+        removeTodo,
+        toggle,
+      }}
+    >
+      {props.children}
+    </TodoContext.Provider>
+  );
+};
+
+export default TodoProvider;
