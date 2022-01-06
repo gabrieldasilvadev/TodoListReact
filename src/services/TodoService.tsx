ff0d02e47b5d1a12c15f8a6f6d51f7f60cc9b6f0
gabriedasilvadev <gabrieldasilvaprivado1@gmail.com>
2022-01-06 16:08:56 -0300

    Create service for Todo

---
+++ src/services/TodoService.tsx
@@ -0,0 +1,16 @@
+import { Todo } from "../models/Todo";
+
+const TODO_STORE = "";
+
+export const get = (): Todo[] => {
+  const data = localStorage.getItem(TODO_STORE) || "";
+  try {
+    const result = JSON.parse(data) as Todo[];
+  } catch (error) {
+    return [];
+  }
+};
+
+export const save = (data: Todo[]) => {
+  if (data?.length >= 1) localStorage.setItem(TODO_STORE, JSON.stringify(data));
+};
